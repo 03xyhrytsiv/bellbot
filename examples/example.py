5f081bc66167f32787317a387ef91109e9ea58ca
03xyhrytsiv <36604665+03xyhrytsiv@users.noreply.github.com>
2018-03-27 13:40:47 +0300

    Add files via upload

---
+++ examples/example.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+import json
+
+
+data = urlopen("http://95.46.45.206:8000/public/goods").read()
+data = json.loads(data)
+europeanKitchen = []
+japaneseKitchen = []
+desserts = []
+drinks = []
+pisneMenu = []
+with open("jsonFile.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+for dicts in data:
+    if dicts['subCategoryResponse']['categoryResponse']['name']\
+            == 'Європейська Кухня':
+        europeanKitchen.append(dicts)
+    if dicts['subCategoryResponse']['categoryResponse']['name'] \
+            == 'Десерти':
+        desserts.append(dicts)
+    if dicts['subCategoryResponse']['categoryResponse']['name'] \
+            == 'Напої':
+        drinks.append(dicts)
+    if dicts['subCategoryResponse']['categoryResponse']['name']\
+            == 'Пісне Меню':
+        pisneMenu.append(dicts)
+    if dicts['subCategoryResponse']['categoryResponse']['name'] \
+            == 'Японська Кухня':
+        japaneseKitchen.append(dicts)
